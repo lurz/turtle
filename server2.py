c331d4bbe6ffbf26d246b4c7eb9a8b18f288b7f0
lurz <lurz@umich.edu>
2018-05-02 12:46:22 -0700

    in server

--- server2.py
+++ server2.py
@@ -1,28 +1,19 @@
-import logging
 from websocket_server import WebsocketServer
 import rospy
 from geometry_msgs.msg import Twist
-import goforward
 
-class GoForward():
-    def __init__(self):
-        rospy.init_node('GoForward', anonymous=False)
-        rospy.loginfo("To stop TurtleBot CTRL + C")
-        rospy.on_shutdown(self.shutdown)
-        self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
-        r = rospy.Rate(10)
-        move_cmd = Twist()
-        move_cmd.linear.x = 0.2
-        move_cmd.angular.z = 0
-        while not rospy.is_shutdown():
-            self.cmd_vel.publish(move_cmd)
-            r.sleep()
+def GoForward():
+    rospy.init_node('GoForward', anonymous=False)
+    rospy.loginfo("To stop TurtleBot CTRL + C")
+    cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
+    r = rospy.Rate(10)
+    move_cmd = Twist()
+    move_cmd.linear.x = 0.2
+    move_cmd.angular.z = 0
+    while not rospy.is_shutdown():
+        self.cmd_vel.publish(move_cmd)
+        r.sleep()
                         
-    def shutdown(self):
-        rospy.loginfo("Stop TurtleBot")
-        self.cmd_vel.publish(Twist())
-        rospy.sleep(1)
-
 def new_client(client, server):
     server.set_fn_message_received(message_received)
 
@@ -30,7 +21,7 @@ def message_received(client, server, message):
     print(message)
     if message == "forward":
         try:
-            goforward
+            GoForward()
         except:
             rospy.loginfo("GoForward node terminated.")
     server.send_message_to_all("succeed!")
